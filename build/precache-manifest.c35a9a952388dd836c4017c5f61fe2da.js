9e8c163d1fe38e52308b49abf3d96edb1a0b5b63
PRIAMBUDI LINTANG BAGASKARA <priambudi.lintang@ui.ac.id>
2019-09-22 21:13:09 +0700

    refactor

---
+++ build/precache-manifest.c35a9a952388dd836c4017c5f61fe2da.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59a2b4232b4159c7a8288ce79a720104",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b787e7d34701ab7b784",
+    "url": "/static/css/main.d650a936.chunk.css"
+  },
+  {
+    "revision": "da7a3c80c5e88e801411",
+    "url": "/static/js/2.cec3d458.chunk.js"
+  },
+  {
+    "revision": "8b787e7d34701ab7b784",
+    "url": "/static/js/main.24d59d97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "13dc531a2ffd9ca7eeb500515774fa74",
+    "url": "/static/media/GlacialIndifference.13dc531a.otf"
+  },
+  {
+    "revision": "f801e234d51049bd538d3d95f6f4e110",
+    "url": "/static/media/GlacialIndifferenceBold.f801e234.otf"
+  },
+  {
+    "revision": "04c38a99d8afcbb092591a7a88a39a88",
+    "url": "/static/media/Kollektif-Bold.04c38a99.ttf"
+  },
+  {
+    "revision": "4d49be6e42d18464b230d87d1fab175a",
+    "url": "/static/media/Kollektif.4d49be6e.ttf"
+  },
+  {
+    "revision": "59df8844491f9d043243b8df2bcfa17c",
+    "url": "/static/media/Oshare-Black-1.59df8844.png"
+  }
+]);
